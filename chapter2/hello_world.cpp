a8cfdf61497a4d531aff553b6ea1ffe2ea06fb03
Edward NoaLand <771818272@qq.com>
2022-01-10 10:25:30 +0800

    feat: answer for chapter 2 question 1

---
+++ chapter2/hello_world.cpp
@@ -0,0 +1,10 @@
+#include<std_lib_facilities.h>
+
+int main() {
+    // exercise 1
+    cout << "Hello, programming!" << endl;
+    cout << "Here we go!" << endl;
+    keep_window_open();
+
+    return 0;
+}
---
+++ chapter2/std_lib_facilities.h
@@ -0,0 +1,17 @@
+#ifndef PRINCIPLES_AND_PRACTICE_USING_CPP_STD_LIB_FACILITIES_H
+#define PRINCIPLES_AND_PRACTICE_USING_CPP_STD_LIB_FACILITIES_H
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+inline void keep_window_open() {
+    char ch;
+    cin >> ch;
+}
+
+#endif //PRINCIPLES_AND_PRACTICE_USING_CPP_STD_LIB_FACILITIES_H
