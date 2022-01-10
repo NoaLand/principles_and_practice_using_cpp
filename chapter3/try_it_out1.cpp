ee875c1a81bd0b74d26235cf333095cf0bbed83d
Edward NoaLand <771818272@qq.com>
2022-01-10 11:09:14 +0800

    feat: add new feature on repeat words checking

--- chapter3/try_it_out1.cpp
+++ chapter3/try_it_out1.cpp
@@ -9,11 +9,14 @@ int main() {
 //    cout << "Hello, " << first_name << "(age " << age * 12 << " months)\n";
 
     // try it out 2
+    int number_of_words = 0;
     string previous = "";
     string current;
     while(cin >> current) {
         if(previous == current) {
-            cout << "repeated word: " << current << endl;
+            ++number_of_words;
+            cout << "word number: " << number_of_words
+                 << " repeated: " << current << endl;
         }
 
         previous = current;
