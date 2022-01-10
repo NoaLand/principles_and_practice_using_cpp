d8570ef4468256f6c7973cc35ebd4cd4b72d8dd2
Edward NoaLand <771818272@qq.com>
2022-01-10 11:04:18 +0800

    feat: add repeated word checking test in try it out

--- chapter3/try_it_out1.cpp
+++ chapter3/try_it_out1.cpp
@@ -1,11 +1,23 @@
 #include<../lib/std_lib_facilities.h>
 
 int main() {
-    cout << "Please enter your first name and age:\n";
-    string first_name = "???";
-    double age = -1.0;
-    cin >> first_name >> age;
-    cout << "Hello, " << first_name << "(age " << age * 12 << " months)\n";
+    // try it out 1
+//    cout << "Please enter your first name and age:\n";
+//    string first_name = "???";
+//    double age = -1.0;
+//    cin >> first_name >> age;
+//    cout << "Hello, " << first_name << "(age " << age * 12 << " months)\n";
+
+    // try it out 2
+    string previous = "";
+    string current;
+    while(cin >> current) {
+        if(previous == current) {
+            cout << "repeated word: " << current << endl;
+        }
+
+        previous = current;
+    }
 
     return 0;
 }
