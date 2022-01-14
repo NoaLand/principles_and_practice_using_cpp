ad9c37a7ef281a123f687a3fe5df98e2653c59ac
Edward NoaLand <771818272@qq.com>
2022-01-14 14:09:40 +0800

    feat: finish square without using times

--- chapter4/chapter4.cpp
+++ chapter4/chapter4.cpp
@@ -6,12 +6,30 @@ void try_it_out_print_char_and_num_for_version();
 
 void print_char_range(char start_char, char end_char);
 
+void try_it_out_square_without_times();
+
+int my_square(int num);
+
 int main() {
     // try_it_out_print_char_and_num_while_version();
-    try_it_out_print_char_and_num_for_version();
+    // try_it_out_print_char_and_num_for_version();
+    try_it_out_square_without_times();
 
     keep_window_open();
     return 0;
+}
+
+void try_it_out_square_without_times() {
+    int res = my_square(9);
+    cout << "res: " << res << endl;
+}
+
+int my_square(int num) {
+    int res{0};
+    for(int index = 0; index < num; ++index) {
+        res += num;
+    }
+    return res;
 }
 
 void try_it_out_print_char_and_num_for_version() {
@@ -27,7 +45,7 @@ void print_char_range(char start_char, char end_char) {
 }
 
 void try_it_out_print_char_and_num_while_version() {
-    int start = 0;
+    int start{0};
     while (int{'a'} + start <= 122) {
         char c = char('a' + start);
         cout << c << "\t" << int{c} << endl;
