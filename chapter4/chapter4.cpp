92d3005ce43aaa9dcdddacc7af32e22f4877ab0d
Edward NoaLand <771818272@qq.com>
2022-01-14 13:53:22 +0800

    feat: finish try it out 1

--- chapter4/chapter4.cpp
+++ chapter4/chapter4.cpp
@@ -1,7 +1,19 @@
 #include <../lib/std_lib_facilities.h>
 
-int main() {
-    keep_window_open();
+void try_it_out_print_char_and_num();
 
-    return 0
+int main() {
+    try_it_out_print_char_and_num();
+
+    keep_window_open();
+    return 0;
+}
+
+void try_it_out_print_char_and_num() {
+    int start = 0;
+    while (int{'a'} + start <= 122) {
+        char c = char('a' + start);
+        cout << c << "\t" << int{c} << endl;
+        ++start;
+    }
 }
