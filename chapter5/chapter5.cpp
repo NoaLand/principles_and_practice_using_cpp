8ab8ff39f42cee318cee93468e7b039a66e945f1
Edward NoaLand <771818272@qq.com>
2022-01-19 11:40:16 +0800

    refactor: use english

--- chapter5/chapter5.cpp
+++ chapter5/chapter5.cpp
@@ -12,10 +12,10 @@ int main() {
 }
 
 void try_it_out_1() {
-    // int s1 = area(7;   // ) 丢失
-    // int s2 = area(7)   // ; 丢失
-    // Int s3 = area(7);  // Int 不是数据类型
-    // int s4 = area('7); // 非终止符（' 丢失）
+    // int s1 = area(7;   // symbol ) lost
+    // int s2 = area(7)   // symbol ; lost
+    // Int s3 = area(7);  // Int is not a type
+    // int s4 = area('7); // symbol ' lost
 }
 
 int area(int length, int width) {
