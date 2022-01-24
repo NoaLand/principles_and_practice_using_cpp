497e02e0f3bfb592444ab2e8751e09ec329466b4
Edward NoaLand <771818272@qq.com>
2022-01-24 19:06:36 +0800

    feat: implement calculate multiple operands, but still has a bug, which is the calculate order

--- chapter6/calculator.cpp
+++ chapter6/calculator.cpp
@@ -1,18 +1,33 @@
 #include<../lib/std_lib_facilities.h>
 
 int main() {
-    cout << "Please enter expression (we can handle + and -):";
-    int lval{0}, rval, res{0};
-    char op;
-    cin >> lval >> op >> rval;
+    cout << "Please enter expression (we can handle + and -):\n";
+    cout << "add an x to end expression (e.g., 1 + 2 * 3 x):\n";
+    int lval{0}, rval;
+    cin >> lval;
+    if(!cin) simple_error("no first operand");
+    for(char op; cin >> op;) {
+        if(op != 'x') cin >> rval;
+        if(!cin) simple_error("no second operand");
+        switch(op) {
+            case '+':
+                lval += rval;
+                break;
+            case '-':
+                lval -= rval;
+                break;
+            case '*':
+                lval *= rval;
+                break;
+            case '/':
+                lval /= rval;
+                break;
+            default:
+                cout << "Result: " << lval << endl;
+                keep_window_open();
+                return 0;
+        }
+    }
 
-    if(op == '+')
-        res = lval + rval;
-    else if(op == '-')
-        res = lval - rval;
-
-    cout << "Result: " << res << endl;
-
-    keep_window_open();
-    return 0;
+    simple_error("bad expression");
 }
