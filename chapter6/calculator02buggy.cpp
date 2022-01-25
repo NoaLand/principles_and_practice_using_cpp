02d6a92d67374fa45e20754d6854a8eaaf79c050
Edward NoaLand <771818272@qq.com>
2022-01-25 19:22:36 +0800

    feat: finish exercise 6, add support for {}

--- chapter6/calculator02buggy.cpp
+++ chapter6/calculator02buggy.cpp
@@ -73,7 +73,7 @@ Token Token_stream::get() {
     switch (ch) {
         case '=':    // for "print"
         case 'x':    // for "quit"
-        case '(': case ')': case '+': case '-': case '*': case '/':
+        case '(': case ')': case '{': case '}': case '+': case '-': case '*': case '/':
             return {ch};        // let each character represent itself
         case '.':
         case '0': case '1': case '2': case '3': case '4':
@@ -103,11 +103,16 @@ double expression();    // declaration so that primary() can call expression()
 double primary() {
     Token t = ts.get();
     switch (t.kind) {
-        case '(':    // handle '(' expression ')'
-        {
+        case '(': {    // handle '(' expression ')'
             double d = expression();
             t = ts.get();
-            if (t.kind != ')') simple_error("')' expected");
+            if(t.kind != ')') simple_error("')' expected");
+            return d;
+        }
+        case '{': {
+            double d = expression();
+            t = ts.get();
+            if(t.kind != '}') simple_error("'}' expected");
             return d;
         }
         case '8':            // we use '8' to represent a number
