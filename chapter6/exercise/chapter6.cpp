d1f867fa77fc6c125ed4887b573020a59d1397e4
Edward NoaLand <771818272@qq.com>
2022-01-30 21:47:40 +0800

    feat: finish exercise 9, print num to string, and implement a very tricky function which needs to improve and refactor

--- chapter6/exercise/chapter6.cpp
+++ chapter6/exercise/chapter6.cpp
@@ -26,6 +26,37 @@ bool Name_value::is_duplicate(const Name_value &nv) {
     return this->name == nv.name;
 }
 
+class Digit {
+public:
+    Digit(string, int);
+    void set_value(int);
+    string to_string() const;
+    bool is_zero() const;
+private:
+    string place;
+    int value{0};
+};
+
+Digit::Digit(string p, int v): place(std::move(p)), value(v) {
+}
+
+void Digit::set_value(int v) {
+    this->value = v;
+}
+
+string Digit::to_string() const {
+    string res;
+    if(value == 0) res = "";
+    if(value == 1) res = ::to_string(value) + " " + place + " ";
+    if(value > 1) res = ::to_string(value) + " " + place + "s ";
+
+    return res;
+}
+
+bool Digit::is_zero() const {
+    return value == 0;
+}
+
 void exercise_4_rewrite_chapter4_exercise_19_with_class();
 
 [[noreturn]] void exercise_8_rewrite_chapter5_bulls_and_cows_game_to_support_input_chars();
@@ -34,14 +65,96 @@ vector<char> generate_rand_chars();
 
 void test_for_generate_random_chars();
 
+[[noreturn]] void exercise_9_get_num_and_print_in_string();
+
+void input_nums_should_less_than_9999(const vector<char> &input_num);
+
+vector<Digit> init_four_digits();
+
+char get_int(char v);
+
+bool add_and_rule_1(vector<Digit> &four_digits, int index);
+
+void print_to_num_string(vector<Digit> &four_digits, vector<char> &input_nums);
+
 int main() {
     // exercise_4_rewrite_chapter4_exercise_19_with_class();
     // exercise_8_rewrite_chapter5_bulls_and_cows_game_to_support_input_chars();
-    test_for_generate_random_chars();
+    // test_for_generate_random_chars();
+    exercise_9_get_num_and_print_in_string();
 
     keep_window_open();
     return 0;
 }
+
+[[noreturn]] void exercise_9_get_num_and_print_in_string() {
+    while(true) {
+        vector<Digit> four_digits = init_four_digits();
+        cout << "Please enter the num: ";
+        vector<char> input_nums;
+
+        while(cin) {
+            input_nums_should_less_than_9999(input_nums);
+
+            char num_char;
+            cin >> num_char;
+
+            if(num_char == ';') break;
+
+            input_nums.push_back(num_char);
+        }
+
+        int offset = four_digits.size() - input_nums.size(); // NOLINT(cppcoreguidelines-narrowing-conversions)
+
+        for(int index = 3; index >= offset; --index) {
+            int position_in_char = index - offset;
+            char v = input_nums.at(position_in_char);
+            four_digits.at(index).set_value(get_int(v));
+        }
+
+        print_to_num_string(four_digits, input_nums);
+
+    }
+}
+
+// TODO: tricky method, need to improve if have time later
+void print_to_num_string(vector<Digit> &four_digits, vector<char> &input_nums) {
+    // print input num
+    for(char num: input_nums) {
+        cout << num;
+    }
+    cout << " is ";
+
+    vector<Digit> format_digit_num;
+    // remove 0 value digit
+    for(auto & digit : four_digits) {
+        if(!digit.is_zero())
+            format_digit_num.push_back(digit);
+    }
+
+    // insert "and" in format digit vector
+    for(int index = 0; index < format_digit_num.size() - 1; ++index) {
+        cout << format_digit_num.at(index).to_string();
+        cout << "and ";
+
+    }
+    // print the last digit vector element
+    cout << format_digit_num.at(format_digit_num.size() - 1).to_string();
+    cout << endl;
+}
+
+char get_int(char v) { return v - '0'; }
+
+vector<Digit> init_four_digits() {
+    return {
+            {"thousand", 0},
+            {"hundred", 0},
+            {"ten", 0},
+            {"one", 0},
+    };
+}
+
+void input_nums_should_less_than_9999(const vector<char> &input_num) { if(input_num.size() > 4) simple_error("cannot input num bigger than 9999"); }
 
 void test_for_generate_random_chars() {
     for(int index = 0; index < 1000000; ++index) {
