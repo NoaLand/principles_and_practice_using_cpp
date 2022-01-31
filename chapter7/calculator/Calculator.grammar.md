08092a2b2eb8a11fe78eaa476423f2690e612df8
Edward NoaLand <771818272@qq.com>
2022-01-31 16:42:17 +0800

    feat: add chapter 6 calculator code

---
+++ chapter7/calculator/Calculator.grammar.md
@@ -0,0 +1,49 @@
+# [Docs] Calculator 1.0
+
+We build a prototype of calculator in chapter 6, and trying to follow the instruction of PPP book to design a basic grammar for this calculator, like following:
+
+To support for factorial calculating, we need to re-design this grammar, which means should add an additional layer between `Term` and `Primary`.
+
+```
+Expression:
+    Term
+    Expression "+" Term
+    Expression "-" Term
+Term:
+    Primary
+    Term "*" Primary
+    Term "/" Primary
+    # Term "%" Primary
+Primary:
+    SubPrimary
+    SubPrimary "!"
+SubPrimary:
+    Number
+    "(" Expression ")"
+    "{" Expression "}"
+    # "P" is for "Permutation", "C" is for "Combination"
+    "P" "(" Expression "," Expression ")"
+    "C" "(" Expression "," Expression ")"
+Number:
+    floating-point-literal
+```
+
+We say, the basic element for an `Expression` is a `Number`, and we borrow the floating number from C++.
+
+Here's an example for the parser above, what will happen when the grammar is trying to parse an `Expression` -- `45 + 11.5 * 7`:
+
+```mermaid
+graph BT
+
+    45[45] --> Num1[Number] --> Pri1[Primary] --> Term1[Term] --> Exp1[Expression] --> ExpTotal[Expression]
+    
+    +[+] ------> ExpTotal[Expression]
+    
+    11.5[11.5] --> Num2[Number] --> Pri2[Primary] --> Term2[Term] --> TermTotal[Term]
+    
+    *[*] -----> TermTotal[Term]
+    
+    7[7] --> Num3[Number] --> Pri3[Primary] ---> TermTotal[Term]
+    
+    TermTotal[Term] --> ExpTotal[Expression]
+```
---
+++ chapter7/calculator/calculator.cpp
@@ -0,0 +1,235 @@
+#include "../../lib/std_lib_facilities.h"
+
+class Token {
+public:
+    Token(char ch)    // make a Token from a char
+            :kind(ch), value(0) { }
+    Token(char ch, double val)     // make a Token from a char and a double
+            :kind(ch), value(val) { }
+    char kind;        // what kind of token
+    double value;     // for numbers: a value
+};
+
+class Token_stream {
+public:
+    Token_stream();
+    Token get();
+    void putback(Token t);
+private:
+    bool full;
+    Token buffer;
+};
+
+Token_stream::Token_stream(): full(false), buffer(0) {
+}
+
+void Token_stream::putback(Token t) {
+    if (full) simple_error("putback() into a full buffer");
+    buffer = t;
+    full = true;
+}
+
+Token Token_stream::get() {
+    if (full) {
+        full = false;
+        return buffer;
+    }
+
+    char ch;
+    cin >> ch;
+
+    switch (ch) {
+        case '=':    // for "print"
+        case 'x':    // for "quit"
+        case '(': case ')': case '{': case '}': case '+': case '-': case '*': case '/': case '!':
+        case 'P': case 'C': case ',':
+            return {ch};        // let each character represent itself
+        case '.':
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        {
+            cin.putback(ch);         // put digit back into the input stream
+            double val;
+            cin >> val;              // read a floating-point number
+            return {'8', val};   // let '8' represent "a number"
+        }
+        default:
+            simple_error("Bad token");
+    }
+}
+
+Token_stream ts;
+
+double expression();
+
+double factorial(double left);
+
+double permutation(double a, double b);
+
+double combination(double a, double b);
+
+double sub_primary() {
+    Token t = ts.get();
+    switch (t.kind) {
+        case '(': {
+            double d = expression();
+            t = ts.get();
+            if(t.kind != ')') simple_error("')' expected");
+            return d;
+        }
+        case '{': {
+            double d = expression();
+            t = ts.get();
+            if(t.kind != '}') simple_error("'}' expected");
+            return d;
+        }
+        case '8':
+            return t.value;
+        case 'P': {
+            t = ts.get();
+            if(t.kind != '(') simple_error("Permutation should has () statement");
+            double a = expression();
+            t = ts.get();
+            if(t.kind != ',') simple_error("there should be a ',' after expression A");
+            double b = expression();
+            t = ts.get();
+            if(t.kind != ')') simple_error("Permutation should has () statement");
+
+            return permutation(a, b);
+        }
+        case 'C': {
+            t = ts.get();
+            if(t.kind != '(') simple_error("Combination should has () statement");
+            double a = expression();
+            t = ts.get();
+            if(t.kind != ',') simple_error("there should be a ',' after expression A");
+            double b = expression();
+            t = ts.get();
+            if(t.kind != ')') simple_error("Combination should has () statement");
+
+            return combination(a, b);
+        }
+        default:
+            simple_error("primary expected");
+    }
+}
+
+double combination(double a, double b) { return permutation(a, b) / factorial(b); }
+
+double permutation(double a, double b) { return factorial(a) / factorial(a - b); }
+
+double primary() {
+    double left = sub_primary();
+    Token t = ts.get();
+
+    while(true) {
+        switch(t.kind) {
+            case '!': {
+                left = factorial(left);
+                t = ts.get();
+                break;
+            }
+            default:
+                ts.putback(t);     // put t back into the token stream
+                return left;
+        }
+    }
+}
+
+double factorial(double left) {
+    int integer_left = (int)left;
+    // will get into this statement when left value is a double
+    if(integer_left != left) simple_error("cannot handle double in factorial");
+    if(integer_left == 0){
+        left = 1; } else { int res = 1;
+        for(int index = 1; index <= left; ++index) {
+            res *= index;
+        }
+        left = res;
+    }
+    return left;
+}
+
+double term() {
+    double left = primary();
+    Token t = ts.get();
+
+    while(true) {
+        switch (t.kind) {
+            case '*':
+                left *= primary();
+                t = ts.get();
+                break;
+            case '/': {
+                double d = primary();
+                if (d == 0) simple_error("divide by zero");
+                left /= d;
+                t = ts.get();
+                break;
+            }
+            default:
+                ts.putback(t);     // put t back into the token stream
+                return left;
+        }
+    }
+}
+
+double expression() {
+    double left = term();
+    Token t = ts.get();
+
+    while(true) {
+        switch (t.kind) {
+            case '+':
+                left += term();
+                t = ts.get();
+                break;
+            case '-':
+                left += term();
+                t = ts.get();
+                break;
+            default:
+                ts.putback(t);
+                return left;
+        }
+    }
+}
+
+int main() {
+    try {
+        double val{0};
+        cout << "------------------------------------------------------------------------------" << endl;
+        cout << "Welcome to our simple calculator." << endl;
+        cout << "Please enter expressions using floating-point numbers." << endl;
+        cout << "1. You can use +, -, *, /, % to calculate." << endl;
+        cout << "2. You can calculate Permutation by inputting P(a, b) " << endl;
+        cout << "3. You can calculate Combination by inputting C(a, b) " << endl;
+        cout << "4. By inputting '=' to print the result of expression you have entered." << endl;
+        cout << "5. By inputting 'x' to exit this calculator." << endl;
+        cout << "------------------------------------------------------------------------------" << endl;
+        while (cin) {
+            Token t = ts.get();
+
+            if (t.kind == 'x') break;
+            if (t.kind == '=') {
+                cout << "=" << val << '\n';
+                continue;
+            }
+            else {
+                ts.putback(t);
+            }
+            val = expression();
+        }
+        keep_window_open();
+    }
+    catch (exception &e) {
+        cerr << "error: " << e.what() << '\n';
+        keep_window_open();
+        return 1;
+    }
+    catch (...) {
+        cerr << "Oops: unknown exception!\n";
+        keep_window_open();
+        return 2;
+    }
+}
