45058a0569c3ab3f64059c58b364c3566c66be71
Edward NoaLand <771818272@qq.com>
2022-02-03 13:36:31 +0800

    fix: wrong construct when cin quit to exit calculator

--- chapter7/calculator/calculator08buggy.cpp
+++ chapter7/calculator/calculator08buggy.cpp
@@ -75,7 +75,7 @@ Token Token_stream::get()
                 while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch;
                 cin.unget();
                 if (s == "let") return Token(let);
-                if (s == "quit") return Token(name);
+                if (s == "quit") return Token(quit);
                 return Token(name, s);
             }
             simple_error("Bad token");
