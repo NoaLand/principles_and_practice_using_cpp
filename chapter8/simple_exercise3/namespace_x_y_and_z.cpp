b5788a3436daffdfc7ffe20d8c48901c119c15a1
Edward NoaLand <771818272@qq.com>
2022-02-09 18:17:42 +0800

    feat: finish simple exercise 3, by creating namespace X, Y and Z, and implement var and print() in each namespace

--- chapter8/simple_exercise3/namespace_x_y_and_z.cpp
+++ chapter8/simple_exercise3/namespace_x_y_and_z.cpp
@@ -1,6 +1,41 @@
 #include<../lib/std_lib_facilities.h>
 
+namespace X {
+    int var;
+    void print() {
+        cout << "X::var: " << var << endl;
+    }
+}
+
+namespace Y {
+    int var;
+    void print() {
+        cout << "Y::var: " << var << endl;
+    }
+}
+
+namespace Z {
+    int var;
+    void print() {
+        cout << "Z::var: " << var << endl;
+    }
+}
+
 int main() {
+    X::var = 7; // set 7 to var in X
+    X::print(); // print var in X
+    using namespace Y;
+    var = 9; // set 9 to var in Y
+    print(); // print var in Y
+    {
+        using Z::var;
+        using Z::print;
+        var == 11; // set 11 to var in Z
+        print(); // print var in Z
+    }
+    print(); // print var in Y, since `using namespace Y`
+    X::print(); // print var in X
+
     keep_window_open();
     return 0;
 }
