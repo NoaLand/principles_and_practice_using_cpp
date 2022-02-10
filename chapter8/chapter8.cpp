0b063826590ab3cee2468b1a8db9753bf46c945c
Edward NoaLand <771818272@qq.com>
2022-02-10 22:12:46 +0800

    feat: finish exercise 5, create 2 method to swap int vector, by using new vector and by using swap function

--- chapter8/chapter8.cpp
+++ chapter8/chapter8.cpp
@@ -8,13 +8,51 @@ void exercise_3_fibonacci();
 
 void fibonacci(int x, int y, vector<int> &fib, int n);
 
+void exercise_5_swap_vector();
+
+void swap_vector_by_creating_new_vector(const vector<int> &v1, vector<int> &v2);
+
+void swap_vector_by_using_swap_num(vector<int> &v1);
+
+void my_swap(int &n1, int &n2);
+
 int main() {
     // exercise_2_print_int_vector_with_label();
-    exercise_3_fibonacci();
+    // exercise_3_fibonacci();
     // exercise 4, the approximate int max value is 1836311903, the n in fibonacci is 46
+    exercise_5_swap_vector();
     
     keep_window_open();
     return 0;
+}
+
+void exercise_5_swap_vector() {
+    vector<int> original_v{1, 3, 5, 7, 9};
+    vector<int> new_v;
+    swap_vector_by_creating_new_vector(original_v, new_v);
+    print("new v is: ", new_v);
+
+    vector<int> v{1, 3, 5, 7, 9};
+    swap_vector_by_using_swap_num(v);
+    print("v is: ", v);
+}
+
+void swap_vector_by_using_swap_num(vector<int> &v1) {
+    for(int index = 0; index < v1.size() / 2; ++index) {
+        my_swap(v1.at(index), v1.at(v1.size() - index - 1));
+    }
+}
+
+void my_swap(int &n1, int &n2) {
+    int temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+
+void swap_vector_by_creating_new_vector(const vector<int> &v1, vector<int> &v2) {
+    for(int index = v1.size() - 1; index >= 0; --index) {
+        v2.push_back(v1.at(index));
+    }
 }
 
 void exercise_3_fibonacci() {
