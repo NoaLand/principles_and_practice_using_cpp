5a696787a23d57f4657a35e22f59d2d1295de4e7
Edward NoaLand <771818272@qq.com>
2022-02-10 21:02:22 +0800

    refactor: create variable file for variable and symbol table

--- chapter8/calculator/calculator.cpp
+++ chapter8/calculator/calculator.cpp
@@ -1,6 +1,7 @@
 #include <utility>
 
-#include "../../lib/std_lib_facilities.h"
+#include "calculator/variable/variable.h"
+//#include "../../lib/std_lib_facilities.h"
 
 const char name = 'a';
 
@@ -38,63 +39,6 @@ int narrow_cast_to_int(double d) {
     return d_int;
 }
 
-class Variable {
-public:
-    string name;
-    double value;
-    bool is_const;
-};
-
-class Symbol_table {
-public:
-    double get_value(const string&);
-    void set_value(const string&, double);
-    bool is_declared(const string&);
-    double define_name(const string&, double, bool);
-private:
-    vector<Variable> var_table;
-};
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wreturn-type"
-double Symbol_table::get_value(const string& s) {
-    for(const Variable& v: var_table) {
-        if(v.name == s) {
-            return v.value;
-        }
-    }
-    simple_error("get: undefined variable " + s);
-}
-#pragma clang diagnostic pop
-
-void Symbol_table::set_value(const string& s, double d) {
-    for(Variable& v: var_table) {
-        if(v.name == s) {
-            if(!v.is_const) {
-                v.value = d;
-                return;
-            } else {
-                simple_error("set: variable is const, cannot change");
-            }
-        }
-    }
-    simple_error("set: undefined variable " + s);
-}
-
-bool Symbol_table::is_declared(const string& var) {
-    return std::any_of(
-            var_table.begin(),
-            var_table.end(),
-            [var](const Variable& v){return v.name == var;}
-            );
-}
-
-double Symbol_table::define_name(const string& var, double val, bool is_const = false) {
-    if(is_declared(var)) simple_error(var + " declared twice");
-    var_table.push_back({var, val, is_const});
-
-    return val;
-}
 
 class Token {
 public:
---
+++ chapter8/calculator/variable/variable.h
@@ -0,0 +1,20 @@
+#ifndef PRINCIPLES_AND_PRACTICE_USING_CPP_VARIABLE_H
+#define PRINCIPLES_AND_PRACTICE_USING_CPP_VARIABLE_H
+#include"../../../lib/std_lib_facilities.h"
+class Variable {
+public:
+    string name;
+    double value;
+    bool is_const;
+};
+
+class Symbol_table {
+public:
+    double get_value(const string&);
+    void set_value(const string&, double);
+    bool is_declared(const string&);
+    double define_name(const string&, double, bool=false);
+private:
+    vector<Variable> var_table;
+};
+#endif //PRINCIPLES_AND_PRACTICE_USING_CPP_VARIABLE_H
