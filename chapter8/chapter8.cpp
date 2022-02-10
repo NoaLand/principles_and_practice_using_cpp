afea016f6066f586bb133c73eb7b81d3d91c3685
Edward NoaLand <771818272@qq.com>
2022-02-10 21:25:29 +0800

    feat: finish exercise 2, print vector with string label

--- chapter8/chapter8.cpp
+++ chapter8/chapter8.cpp
@@ -1,6 +1,25 @@
 #include<../lib/std_lib_facilities.h>
 
+void exercise_2_print_int_vector_with_label();
+
+void print(string &label, vector<int> &iv);
+
 int main() {
+    exercise_2_print_int_vector_with_label();
+    
     keep_window_open();
     return 0;
+}
+
+void exercise_2_print_int_vector_with_label() {
+    string label{"this is a label"};
+    vector<int> iv{1, 2, 3, 4, 5};
+    print(label, iv);
+}
+
+void print(string &label, vector<int> &iv) {
+    cout << label << endl;
+    for(int i: iv) {
+        cout << i << endl;
+    }
 }
