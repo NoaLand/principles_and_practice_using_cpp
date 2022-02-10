fb368c61493338bff21a96a99fdbdea35352d694
Edward NoaLand <771818272@qq.com>
2022-02-10 21:35:54 +0800

    feat: finish exercise 3, create fibonacci function and use print in exercise 2 to print vector

--- chapter8/chapter8.cpp
+++ chapter8/chapter8.cpp
@@ -4,11 +4,33 @@ void exercise_2_print_int_vector_with_label();
 
 void print(const string &label, const vector<int> &iv);
 
+void exercise_3_fibonacci();
+
+void fibonacci(int x, int y, vector<int> &fib, int n);
+
 int main() {
-    exercise_2_print_int_vector_with_label();
+    // exercise_2_print_int_vector_with_label();
+    exercise_3_fibonacci();
     
     keep_window_open();
     return 0;
+}
+
+void exercise_3_fibonacci() {
+    int x{1}, y{1};
+    int n{20};
+    vector<int> v{x, y};
+    fibonacci(x, y, v, n);
+    const string label = "the fibs: ";
+    print(label, v);
+}
+
+void fibonacci(int x, int y, vector<int> &fib, int n) {
+    while(fib.size() < n) {
+        int z = x + y;
+        fib.push_back(z);
+        fibonacci(y, z, fib, n);
+    }
 }
 
 void exercise_2_print_int_vector_with_label() {
