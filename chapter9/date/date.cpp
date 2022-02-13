dff7fa28c19e7efdfefdecf8dbb713d107f518f8
Edward NoaLand <771818272@qq.com>
2022-02-13 20:04:34 +0800

    refactor: refactor Date from struct to class

--- chapter9/date/date.cpp
+++ chapter9/date/date.cpp
@@ -1,9 +1,13 @@
 #include "../lib/std_lib_facilities.h"
 
-struct Date {
+class Date {
     int y, m, d;
+public:
     Date(int y, int m, int d);
     void add_day(int n);
+    int month() { return m; }
+    int day() { return d; }
+    int year() { return y; }
 };
 
 Date::Date(int y, int m, int d): y{y}, m{m}, d{d} {
