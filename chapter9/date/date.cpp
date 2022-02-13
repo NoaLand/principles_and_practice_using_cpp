e91e46d29f6c57f30d7c630c499905ff1cd624a8
Edward NoaLand <771818272@qq.com>
2022-02-13 20:07:37 +0800

    format: reformat class code

--- chapter9/date/date.cpp
+++ chapter9/date/date.cpp
@@ -1,13 +1,15 @@
 #include "../lib/std_lib_facilities.h"
 
 class Date {
-    int y, m, d;
 public:
     Date(int y, int m, int d);
     void add_day(int n);
     int month() { return m; }
     int day() { return d; }
     int year() { return y; }
+    
+private:
+    int y, m, d;
 };
 
 Date::Date(int y, int m, int d): y{y}, m{m}, d{d} {
