6c26f60fbac7989214bf73695471cebc6f13e888
Edward NoaLand <771818272@qq.com>
2022-02-13 20:09:54 +0800

    feat: add init implement for add_day function

--- chapter9/date/date.cpp
+++ chapter9/date/date.cpp
@@ -7,12 +7,15 @@ public:
     int month() { return m; }
     int day() { return d; }
     int year() { return y; }
-    
+
 private:
     int y, m, d;
 };
 
 Date::Date(int y, int m, int d): y{y}, m{m}, d{d} {
+}
+
+void Date::add_day(int n) {
 }
 
 int main() {
