6c042820b4c9fdfdbcd933d30d5618842556c676
Edward NoaLand <771818272@qq.com>
2022-02-13 19:54:25 +0800

    feat: create simple Date struct

--- chapter9/date/date.cpp
+++ chapter9/date/date.cpp
@@ -1,5 +1,13 @@
 #include "../lib/std_lib_facilities.h"
 
+struct Date {
+    int y; // for year
+    int m; // for month
+    int d; // for day
+};
+
+Date today;
+
 int main() {
     keep_window_open();
     return 0;
