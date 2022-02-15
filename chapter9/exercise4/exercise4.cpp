ffa3308be05a59851806f8d438a336b49eac0093
Edward NoaLand <771818272@qq.com>
2022-02-15 13:05:53 +0800

    refactor: add some comment for exercise 4

--- chapter9/exercise4/exercise4.cpp
+++ chapter9/exercise4/exercise4.cpp
@@ -1,10 +1,12 @@
 struct X {
+    // this function will never end
     void f(int x) {
         struct Y {
             int f() { return 1; }
 
             int m;
         };
+        // m and x will never use
         int m;
         m = x;
         Y m2;
@@ -14,14 +16,16 @@ struct X {
     int m;
 
     void g(int m) {
-        if (m) f(m + 2);
-        else {
+        if(m)
+            f(m + 2);
+        else{
             g(m + 2);
         }
     }
 
     X() {}
 
+    // never used
     void m3() {
     }
 
