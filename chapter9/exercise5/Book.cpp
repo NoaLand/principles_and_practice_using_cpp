1fb90ad686fc91d804daffcf9923a99ee30811fa
Edward NoaLand <771818272@qq.com>
2022-02-15 21:19:08 +0800

    refactor: create namespace My_library

--- chapter9/exercise5/Book.cpp
+++ chapter9/exercise5/Book.cpp
@@ -2,43 +2,45 @@
 
 #include <utility>
 
-Book::Book(string isbn, string book_name, string author, string copyright_date):
-    isbn{std::move(isbn)},
-    book_name{std::move(book_name)},
-    author{std::move(author)},
-    copyright_date{std::move(copyright_date)} {
-}
-
-Book::Book(): isbn{""}, book_name{""}, author{""}, copyright_date{""} {
-}
-
-istream &operator>>(istream &is, Book &b) {
-    int n1, n2, n3;
-    char l1, l2, l3, x4;
-    cout << "isbn: ";
-    is >> n1 >> l1 >> n2 >> l2 >> n3 >> l3 >> x4;
-
-    if(!is) return is;
-    if(l1 != '-' || l2 != '-' || l3 != '-' || (!isdigit(x4) && !isalpha(x4))){
-        is.clear(std::ios_base::failbit);
-        return is;
+namespace My_Library {
+    Book::Book(string isbn, string book_name, string author, string copyright_date):
+            isbn{std::move(isbn)},
+            book_name{std::move(book_name)},
+            author{std::move(author)},
+            copyright_date{std::move(copyright_date)} {
     }
 
-    string isbn = to_string(n1) + l1 + to_string(n2) + l2 + to_string(n3) + l3 + to_string(x4);
+    Book::Book(): isbn{""}, book_name{""}, author{""}, copyright_date{""} {
+    }
 
-    string book_name;
-    cout << "book name: ";
-    is >> book_name;
+    istream &operator>>(istream &is, Book &b) {
+        int n1, n2, n3;
+        char l1, l2, l3, x4;
+        cout << "isbn: ";
+        is >> n1 >> l1 >> n2 >> l2 >> n3 >> l3 >> x4;
 
-    string author;
-    cout << "author: ";
-    is >> author;
+        if(!is) return is;
+        if(l1 != '-' || l2 != '-' || l3 != '-' || (!isdigit(x4) && !isalpha(x4))){
+            is.clear(std::ios_base::failbit);
+            return is;
+        }
 
-    string copyright_date;
-    cout << "copyright date: ";
-    is >> copyright_date;
+        string isbn = to_string(n1) + l1 + to_string(n2) + l2 + to_string(n3) + l3 + to_string(x4);
 
-    b = Book{isbn, book_name, author, copyright_date};
+        string book_name;
+        cout << "book name: ";
+        is >> book_name;
 
-    return is;
+        string author;
+        cout << "author: ";
+        is >> author;
+
+        string copyright_date;
+        cout << "copyright date: ";
+        is >> copyright_date;
+
+        b = Book{isbn, book_name, author, copyright_date};
+
+        return is;
+    }
 }
--- chapter9/exercise5/Book.h
+++ chapter9/exercise5/Book.h
@@ -1,29 +1,30 @@
 #ifndef PRINCIPLES_AND_PRACTICE_USING_CPP_BOOK_H
 #define PRINCIPLES_AND_PRACTICE_USING_CPP_BOOK_H
 #include "../lib/std_lib_facilities.h"
+namespace My_Library {
+    class Book {
+    public:
+        class Invalid: exception {};
 
-class Book {
-public:
-    class Invalid: exception {};
+        Book();
+        Book(string isbn, string book_name, string author, string copyright_date);
 
-    Book();
-    Book(string isbn, string book_name, string author, string copyright_date);
+        string get_isbn() const { return isbn; }
+        string get_book_name() const { return book_name; }
+        string get_author() const { return author; }
+        string get_copyright_date() const { return copyright_date; }
+        bool get_is_borrowed() const { return is_borrowed; }
 
-    string get_isbn() const { return isbn; }
-    string get_book_name() const { return book_name; }
-    string get_author() const { return author; }
-    string get_copyright_date() const { return copyright_date; }
-    bool get_is_borrowed() const { return is_borrowed; }
+        void borrowing();
+        void returning();
+    private:
+        string isbn;
+        string book_name;
+        string author;
+        string copyright_date;
+        bool is_borrowed{false};
+    };
 
-    void borrowing();
-    void returning();
-private:
-    string isbn;
-    string book_name;
-    string author;
-    string copyright_date;
-    bool is_borrowed{false};
-};
-
-istream& operator>>(istream& is, Book& b);
+    istream& operator>>(istream& is, Book& b);
+}
 #endif //PRINCIPLES_AND_PRACTICE_USING_CPP_BOOK_H
