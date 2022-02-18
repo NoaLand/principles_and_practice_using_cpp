29b023105912b0209800c7860de6e14a5e00eba9
Edward NoaLand <771818272@qq.com>
2022-02-18 12:04:04 +0800

    feat: implement leap year function

--- chapter9/date/Chrono.cpp
+++ chapter9/date/Chrono.cpp
@@ -53,7 +53,17 @@ namespace Chrono {
     }
 
     bool leapyear(int y) {
-        // ...
+        if(y % 100 == 0) {
+            if(y % 400 == 0) {
+                return true;
+            }
+        } else {
+            if(y % 4 == 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     bool operator==(const Date& a, const Date& b) {
