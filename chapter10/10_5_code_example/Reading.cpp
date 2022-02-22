9ba60ae238b443745dc18921c79003c545f7048a
Edward NoaLand <771818272@qq.com>
2022-02-22 10:52:04 +0800

    feat: add read and write file example code, the file directory is in cmake-build-debug, so need to input relative dir of the file you want to reand and write

---
+++ chapter10/10_5_code_example/Reading.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+#include "../lib/std_lib_facilities.h"
+
+struct Reading {
+    int hour;
+    double temperature;
+};
+
+int main() {
+    cout << "path: " << std::__fs::filesystem::current_path().string() << endl;
+    cout << "please enter input file name: ";
+    string iname;
+    cin >> iname;
+    ifstream ist{iname};
+    if(!ist) simple_error("can't open input file " + iname);
+
+    cout << "path: " << std::__fs::filesystem::current_path().string() << endl;
+    cout << "please enter input file name: ";
+    cout << "please enter output file name: ";
+    string oname;
+    cin >> oname;
+    ofstream ost{oname};
+    if(!ost) simple_error("can't open output file " + oname);
+
+    vector<Reading> temps;
+    int hour;
+    double temperature;
+    while(ist >> hour >> temperature) {
+        if(hour < 0 || hour > 23) simple_error("hour out of range");
+        temps.push_back({hour, temperature});
+    }
+
+    for(Reading r : temps) {
+        ost << '(' << r.hour << ',' << r.temperature << ')' << endl;
+    }
+
+    keep_window_open();
+    return 0;
+}
