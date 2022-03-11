68941d4ef55d4d5f30c900fd570d78d26dc05512
Edward NoaLand <771818272@qq.com>
2022-03-11 11:46:09 +0800

    feat: finish exercise 4, print input data to file

--- chapter10/exercise_2_3/exercise_2_3.cpp
+++ chapter10/exercise_2_3/exercise_2_3.cpp
@@ -1,6 +1,12 @@
 #include "Point.h"
 
 int main() {
+    cout << "please enter your output file name: ";
+    string oname;
+    cin >> oname;
+    ofstream ost{oname};
+    if(!ost) simple_error("wrong ost");
+
     cout << "Please enter 7 points by using the following format (x, y): " << endl;
 
     vector<Point> original_points;
@@ -16,4 +22,13 @@ int main() {
     for(Point p : original_points) {
         cout << p;
     }
+    cout << "FINISH" << endl;
+
+    cout << endl;
+
+    cout << "Print to file..." << endl;
+    for(Point p : original_points) {
+        ost << p;
+    }
+    cout << "FINISH" << endl;
 }
