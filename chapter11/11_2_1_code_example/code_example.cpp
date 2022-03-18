f4b188cd4ead2d1edc48ae20e9b4388a4581e8b9
Edward NoaLand <771818272@qq.com>
2022-03-18 18:37:45 +0800

    feat: add 11.2.1 code example

--- chapter11/11_2_1_code_example/code_example.cpp
+++ chapter11/11_2_1_code_example/code_example.cpp
@@ -1,6 +1,10 @@
 #include "../lib/std_lib_facilities.h"
 
 int main() {
+    cout << 1234 << "\t(decimal)\n"
+         << hex << 1234 << "\t(hexadecimal)\n"
+         << oct << 1234 << "\t(octal)\n";
+    
     keep_window_open();
     return 0;
 }
