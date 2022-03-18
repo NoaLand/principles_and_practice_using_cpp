70b854b74cf1c75723da2d44f81db1cbbbaed1c3
Edward NoaLand <771818272@qq.com>
2022-03-18 18:42:45 +0800

    feat: finish try it out 1 code

---
+++ chapter11/try_it_out_1/try_it_out_1.cpp
@@ -0,0 +1,12 @@
+#include "../lib/std_lib_facilities.h"
+
+int main() {
+    cout << "decimal format:\t" << 1994 << endl;
+    cout << "hexadecimal format:\t" << hex << 1994 << endl;
+    cout << "octal format:\t" << oct << 1994 << endl;
+
+    cout << "my age in decimal:\t" << dec << 28 << endl;
+
+    keep_window_open();
+    return 0;
+}
