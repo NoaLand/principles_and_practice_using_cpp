c19896464199d7e5a519d34530692b7682377612
Edward NoaLand <771818272@qq.com>
2022-03-18 18:39:34 +0800

    feat: add 11.2.1 code example about print rule

--- chapter11/11_2_1_code_example/code_example.cpp
+++ chapter11/11_2_1_code_example/code_example.cpp
@@ -4,7 +4,10 @@ int main() {
     cout << 1234 << "\t(decimal)\n"
          << hex << 1234 << "\t(hexadecimal)\n"
          << oct << 1234 << "\t(octal)\n";
-    
+
+    // this will still stay on octal format
+    cout << 1234 << "\n";
+
     keep_window_open();
     return 0;
 }
