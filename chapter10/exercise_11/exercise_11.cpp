88d78e7d0a9c133c07c6a1d00377e854c04c59c6
Edward NoaLand <771818272@qq.com>
2022-03-18 18:16:29 +0800

    feat: finish exercise 11

--- chapter10/exercise_11/exercise_11.cpp
+++ chapter10/exercise_11/exercise_11.cpp
@@ -1,6 +1,29 @@
 #include "../lib/std_lib_facilities.h"
 
 int main() {
+    cout << "Please enter input file: ";
+    string iname;
+    cin >> iname;
+    ifstream ist{iname};
+    if(!ist) simple_error("wrong ist");
+
+    vector<int> num_vec;
+    for(char num_char; ist >> num_char;) {
+        if(isdigit(num_char)) {
+            ist.putback(num_char);
+            int num;
+            ist >> num;
+            num_vec.push_back(num);
+        }
+    }
+
+    int sum{0};
+    for(int n : num_vec) {
+        sum += n;
+    }
+
+    cout << "sum: " << sum << endl;
+
     keep_window_open();
     return 0;
 }
